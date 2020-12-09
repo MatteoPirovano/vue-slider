c27a3f6d35eebbc69f067c8b65a0272fd6625f27
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2020-12-09 16:24:56 +0100

    Primo commt

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var slider = new Vue(
+  {
+    el : '#root',
+    data : {
+      position : 0,
+      img : [
+        'https://www.greenme.it/wp-content/uploads/2020/09/shutterstock_715342429.jpg',
+        'https://magicblitzen.com/wp-content/uploads/2018/01/lupo-nero.png',
+        'https://i.pinimg.com/474x/2f/70/9b/2f709b47cb45786188374be2915eaf74.jpg',
+        'https://t1.ea.ltmcdn.com/it/images/1/8/6/dingo_canis_lupus_dingo_2681_12_600.jpg',
+        'https://t2.ea.ltmcdn.com/it/images/1/8/6/lupo_dello_yukon_canis_lupus_pambasileus_2681_11_600.jpg',
+      ]
+    },
+    // methods: {
+    //   slide :function () {
+    //   }
+    // }
+  }
+);
